d27a37341c0478fd7d804e32974e06034f89d5f1
tiepbeo222 <kathyrdz20pun0suy@hotmail.com>
2024-05-11 10:15:38 +0700

    update aptos project

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
-const fibonacci = (n) => {
-  if (n <= 1) {
-    return n;
+function canCross(stones) {
+  const map = new Map();
+  for (const stone of stones) map.set(stone, new Set());
+  map.get(0).add(0);
+  for (let i = 0; i < stones.length; i++) {
+    for (const step of map.get(stones[i])) {
+      for (let k = step - 1; k <= step + 1; k++) {
+        if (k > 0 && map.has(stones[i] + k)) map.get(stones[i] + k).add(k);
+      }
+    }
   }
-  let prev = 0;
-  let curr = 1;
-  for (let i = 2; i <= n; i++) {
-    const next = prev + curr;
-    prev = curr;
-    curr = next;
-  }
-  return curr;
-};
+  return map.get(stones[stones.length - 1]).size > 0;
+}
