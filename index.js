0398b66c238f8f08d804d9f6b2f8416b6b4a4ee0
tiepbeo222 <kathyrdz20pun0suy@hotmail.com>
2024-05-14 18:58:38 +0700

    hello commit

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
-function binarySearch(arr, target) {
-  let low = 0;
-  let high = arr.length - 1;
-  while (low <= high) {
-    let mid = Math.floor((low + high) / 2);
-    if (arr[mid] === target) return mid;
-    if (arr[mid] < target) low = mid + 1;
-    else high = mid - 1;
+const countingSort = (arr) => {
+  const min = Math.min(...arr);
+  const max = Math.max(...arr);
+  const count = Array(max - min + 1).fill(0);
+  for (let num of arr) {
+    count[num - min]++;
   }
-  return -1;
-}
+  let sortedIndex = 0;
+  for (let i = min; i <= max; i++) {
+    while (count[i - min] > 0) {
+      arr[sortedIndex++] = i;
+      count[i - min]--;
+    }
+  }
+  return arr;
+};
